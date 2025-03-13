84cebaca90be149202d554cd0952c35cb9371d2e
nelaa7 <nelaanjani22@gmail.com>
2025-03-14 04:37:44 +0700

    admin

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Author extends Model
 {
@@ -17,6 +18,11 @@ class Author extends Model
         'avatar',
         'slug',
     ];
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function news(): HasMany
     {
